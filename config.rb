262e57c59850c597884d80329231d4cd212e21c1
Spencer Bray <spencerjohnbray@gmail.com>
2017-04-10 17:03:26 +0200

    created dynamic pages for flats

--- config.rb
+++ config.rb
@@ -28,8 +28,9 @@ activate :autoprefixer
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+data.flats.each do |owner, flat|
+  proxy "/flats/#{owner}.html", "/flats/show.html", :locals => {:owner => owner }, :ignore => true
+end
 
 ###
 # Helpers
@@ -55,6 +56,8 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+set :partials_dir, 'partials'
 
 # Build-specific configuration
 configure :build do
